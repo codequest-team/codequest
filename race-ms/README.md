69618b87b0254ee8094566e1eed4640e0d945fb6
abu <abu@abu.abu>
2023-03-25 00:44:32 +0600

    add corses

---
+++ race-ms/README.md
@@ -0,0 +1,7 @@
+# Usage
+
+- Переименовать ```.env-example``` в ```.env```, заполнить поля
+- ```pip install -r requirements.txt```
+- ```uvicorn main:app```
+
+swagger доступен по ссылке localhos:8080/docs
--- race-ms/main.py
+++ race-ms/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-from api import router
 from fastapi.middleware.cors import CORSMiddleware
+
+from api import router
+from config import settings
 
 app = FastAPI(
     title='Regex Racing',
     description='CodeQuest Regex Recing game microservice',
     version='1.0.0',
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_HOSTS.split(','),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
