c6524112de41d31adfdcfdd466232eac5083aaad
Maxim Kim <exynil@gmail.com>
2023-03-26 03:51:06 +0600

    fix badge

--- front-ms/src/pages/games/games.vue
+++ front-ms/src/pages/games/games.vue
@@ -17,17 +17,16 @@
           <p class="leading-relaxed">Изучайте и решайте на скорость регулярные выражения со своими друзьями!</p>
         </div>
       </div>
+
       <div class="p-4 md:w-1/4 sm:w-1/2 w-full cursor-pointer">
-        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg hover:bg-indigo-100">
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg hover:bg-indigo-100 relative min-w-96">
+          <q-badge align="top" color="red" label="В разработке" class="absolute top-2 right-2" />
           <svg fill="currentColor" class="text-indigo-500 w-16 h-16 mb-3 inline-block" viewBox="0 0 24 24">
             <path
               d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v13a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13zM1.5 1a.5.5 0 0 0-.5.5V5h4V1H1.5zM5 6H1v4h4V6zm1 4h4V6H6v4zm-1 1H1v3.5a.5.5 0 0 0 .5.5H5v-4zm1 0v4h4v-4H6zm5 0v4h3.5a.5.5 0 0 0 .5-.5V11h-4zm0-1h4V6h-4v4zm0-5h4V1.5a.5.5 0 0 0-.5-.5H11v4zm-1 0V1H6v4h4z"
             />
           </svg>
-          <h2 class="title-font font-medium text-3xl text-gray-900">
-            3 в ряд
-            <q-badge align="top" color="red" label="В разработке" />
-          </h2>
+          <h2 class="title-font font-medium text-3xl text-gray-900 relative">3 в ряд</h2>
 
           <p class="leading-relaxed">
             Играйте в самый популярный формат мобильных игр с пользой для себя! Пишите регулярные выражения, которые
@@ -35,9 +34,10 @@
           </p>
         </div>
       </div>
-      <div class="p-4 md:w-1/4 sm:w-1/2 w-full cursor-pointer">
-        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg hover:bg-indigo-100">
 
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full cursor-pointer">
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg hover:bg-indigo-100 relative min-w-96">
+          <q-badge align="top" color="red" label="В разработке" class="absolute top-2 right-2" />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -53,10 +53,7 @@
             />
           </svg>
 
-          <h2 class="title-font font-medium text-3xl text-gray-900">
-            SQL Race
-            <q-badge align="top" color="red" label="В разработке" />
-          </h2>
+          <h2 class="title-font font-medium text-3xl text-gray-900 relative">SQL Race</h2>
           <p class="leading-relaxed">
             Встречайте всем полюбившийся формат гонок с новым функционалом! Пишите оптимизированные SQL запросы быстрее
             соперников и побеждайте!
